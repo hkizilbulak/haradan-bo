9e70998fec4d7a523495a0354cba82489021d8bf
Sami Yusuf Avcı <samiyusufavci77@gmail.com>
2026-09-06 22:14:59 +0300

    .

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    distDir: process.env.NODE_ENV === 'development' ? '.next-dev' : '.next',
 }
 
 module.exports = nextConfig
